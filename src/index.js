f74da083ffa00f5e94b8d144974da81cf75cba75
omrishooshan <omrishooshan@gmail.com>
2020-04-01 22:51:10 +0300

    editss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const element = React.createElement('h1','hello world',null,'na nach world');
+ReactDOM.render(element,document.getElementById('root'));
