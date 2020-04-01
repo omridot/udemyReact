feb9bf5c8e99cb327d530cb71359ecf96bb1aa57
omrishooshan <omrishooshan@gmail.com>
2020-04-02 00:01:31 +0300

    edit2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React,{component, Component} from 'react'
 import ReactDOM from 'react-dom'
 
-const element = React.createElement('h1','hello world',null,'na nach world');
-ReactDOM.render(element,document.getElementById('root'));
+
+
+
+class List extends Component{
+    render(){
+return <ol>
+{this.props.tasks.map((task,index)=> <li key ={index}>{task}</li>)}
+</ol>
+    }
+
+}
+class Title extends Component{
+    render(){
+    return <h1>{this.props.todo}</h1>
+
+    }
+
+}
+class Main extends Component{
+    render(){
+    return <div>
+      <Title todo={['todos']}/>
+        <List tasks={['omri','maor']}/>
+        <List tasks={['zohar','nachman']}/>
+
+    </div>
+    }
+
+}
+
+
+ReactDOM.render(<Main/>,document.getElementById('root'));
